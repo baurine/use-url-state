a7c9db0226b71ebc17c78bc35f622178c6154653
baurine <2008.hbl@gmail.com>
2024-08-07 14:46:49 +0800

    chore: init demo

--- packages/examples/with-react-router-5/src/App.tsx
+++ packages/examples/with-react-router-5/src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {
+  // BrowserRouter as Router,
+  HashRouter as Router,
+  Link,
+  useLocation
+} from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+// React Router does not have any opinions about
+// how you should parse URL query strings.
+//
+// If you use simple key=value query strings and
+// you do not need to support IE 11, you can use
+// the browser's built-in URLSearchParams API.
+//
+// If your query strings contain array or object
+// syntax, you'll probably need to bring your own
+// query parsing function.
 
+export default function QueryParamsExample() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <QueryParamsDemo />
+    </Router>
   )
 }
 
-export default App
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  const { search } = useLocation()
+
+  return React.useMemo(() => new URLSearchParams(search), [search])
+}
+
+function QueryParamsDemo() {
+  let query = useQuery()
+
+  return (
+    <div>
+      <div>
+        <h2>Accounts</h2>
+        <ul>
+          <li>
+            <Link to="/account?name=netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/account?name=zillow-group">Zillow Group</Link>
+          </li>
+          <li>
+            <Link to="/account?name=yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/account?name=modus-create">Modus Create</Link>
+          </li>
+        </ul>
+
+        <Child name={query.get('name') ?? ''} />
+      </div>
+    </div>
+  )
+}
+
+function Child({ name }: { name: string }) {
+  return (
+    <div>
+      {name ? (
+        <h3>
+          The <code>name</code> in the query string is &quot;{name}
+          &quot;
+        </h3>
+      ) : (
+        <h3>There is no name in the query string</h3>
+      )}
+    </div>
+  )
+}
